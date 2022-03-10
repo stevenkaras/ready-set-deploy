1c4197f01c570e992358eb57c88a576f23d69da0
Steven Karas <steven.karas@gmail.com>
2022-03-10 22:19:11 +0200

    model actual serializes to JSON now

--- ready_set_deploy/model.py
+++ ready_set_deploy/model.py
@@ -1,31 +1,25 @@
 """
 Models for serialized data
 """
+import json
+import dataclasses
 from typing import Optional
-
-class SystemState:
-    def __init__(self):
-        self.subsystems: dict[str, list[SubsystemState]] = {}
+from collections.abc import Iterable
 
 
+@dataclasses.dataclass
 class SubsystemState:
-    def __init__(
-        self,
-        name: str,
-        qualifier: Optional[str] = None,
-        is_partial: bool = False,
-        is_desired: bool = True,
-        after_anchor: Optional[str] = None,
-        before_anchor: Optional[str] = None,
-        elements: list = None,
-    ):
-        self.name = name
-        self.qualifier = qualifier
-        self.is_partial = is_partial
-        self.is_desired = is_desired
-        self.after_anchor = after_anchor
-        self.before_anchor = before_anchor
-        self.elements = elements if elements is not None else []
+    name: str
+    qualifier: Optional[str] = None
+    is_partial: bool = False
+    is_desired: bool = True
+    after_anchor: Optional[str] = None
+    before_anchor: Optional[str] = None
+    elements: list = dataclasses.field(default_factory=list)
+
+    @classmethod
+    def from_dict(cls, source: dict):
+        return cls(**source)
 
     def __str__(self):
         marker = ""
@@ -36,3 +30,36 @@ class SubsystemState:
         return f"<SubsystemState.{self.name} {{{self.qualifier}}} e{marker}={self.elements}>"
 
     __repr__ = __str__
+
+
+@dataclasses.dataclass
+class SystemState:
+    subsystems: dict[str, list[SubsystemState]] = dataclasses.field(default_factory=dict)
+
+    @classmethod
+    def from_dict(cls, source):
+        return cls(
+            subsystems={
+                name: [SubsystemState.from_dict(substate) for substate in substates]
+                for name, substates in source["subsystems"].items()
+            }
+        )
+
+    @classmethod
+    def from_substates(cls, substates: Iterable[SubsystemState]) -> "SystemState":
+        state = cls()
+        for substate in substates:
+            state.subsystems.setdefault(substate.name, []).append(substate)
+
+        return state
+
+
+def _dataclass_to_shallow_dict(obj):
+    return {field.name: getattr(obj, field.name) for field in dataclasses.fields(obj)}
+
+
+class DataclassEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return _dataclass_to_shallow_dict(o)
+        return super().default(o)
