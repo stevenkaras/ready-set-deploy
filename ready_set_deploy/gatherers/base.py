6b13a8573d2a0962b72e39aa6e176d638ace4c79
Steven Karas <steven.karas@gmail.com>
2022-06-23 22:10:29 +0300

    gather/render: utility for gathering and rendering file patches
    at the moment it uses the rsd-patch script. This is strictly inferior to rendering a unified diff for use with
    `patch(1)`, but in the interest of moving forward this is a far simpler approach to build out and test.

--- ready_set_deploy/gatherers/base.py
+++ ready_set_deploy/gatherers/base.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 from ready_set_deploy.components import Component
+from ready_set_deploy.elements import List
 
 
 class Gatherer:
@@ -10,3 +13,16 @@ class Gatherer:
 
     def gather_local(self, *, qualifier: tuple[str, ...] = ()) -> Component:
         raise NotImplementedError("gather_local")
+
+    def gather_file(self, path: str) -> List:
+        """
+        Gather a file into a List with a prefix marker if the file exists
+
+        See the corresponding render_file_diff in the Renderer base class
+        """
+        fullpath = Path(path).expanduser()
+        if not fullpath.exists():
+            return List.zero()
+        else:
+            with open(fullpath, mode="r") as f:
+                return List.infer(["e"] + f.readlines())
--- ready_set_deploy/renderers/base.py
+++ ready_set_deploy/renderers/base.py
@@ -1,8 +1,42 @@
+import json
+import shlex
 from collections.abc import Iterable, Sequence
 
 from ready_set_deploy.components import Component
+from ready_set_deploy.elements import Atom, List, ListDiff, ListDiffOpcode
+from ready_set_deploy.runner import Runner
 
 
 class Renderer:
     def to_commands(self, diff: Component) -> Iterable[Sequence[str]]:
         raise NotImplementedError("to_commands")
+
+    def render_file_diff(self, filepath: str, spec: ListDiff) -> Iterable[Sequence[str]]:
+        """
+        Render the commands for a file diff produced by gather_file in the Gatherer base class
+        """
+        if not spec.diff:
+            return
+
+        raw_op, idx, lhs, rhs = spec.diff[0]
+        op = ListDiffOpcode(raw_op)
+        if idx == 0 and op == ListDiffOpcode.DELETE and lhs == "e":
+            yield from Runner.to_commands(["rm", f"\"{filepath}\""])
+            return
+
+        if idx == 0 and op == ListDiffOpcode.INSERT and rhs == "e":
+            yield from Runner.to_commands(["touch", f"\"{filepath}\""])
+
+        if len(spec.diff) == 1:
+            return
+
+        # the first element ("e") is an existence marker, but it's not part of the file content, so we need to modify the diff manually
+        new_spec = []
+        for op, idx, lhs, rhs in spec.diff:
+            if idx == 0:
+                continue
+            new_spec.append((op, idx - 1, lhs, rhs))
+        fixed = ListDiff(diff=new_spec)
+
+        json_diff = json.dumps(fixed.to_primitive())
+        yield from Runner.to_commands(["rsd-patch", f"\"{filepath}\"", f"{json_diff}"])
